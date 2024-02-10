09f5e170481125cefc80933a44da42e06137a076
Ashis Ranjan Dey <ashisrd@tekion.com>
2024-02-11 00:07:33 +0530

    Add Http codes

--- src/main/java/com/ashis/quizapp/service/QuestionService.java
+++ src/main/java/com/ashis/quizapp/service/QuestionService.java
@@ -3,8 +3,11 @@ package com.ashis.quizapp.service;
 import com.ashis.quizapp.dao.QuestionDao;
 import com.ashis.quizapp.models.Question;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,18 +15,27 @@ public class QuestionService {
 
     @Autowired
     QuestionDao questionDao;
-    public List<Question> getAllQuestions() {
-
-        List<Question> ans = questionDao.findAll();
-        return ans;
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
-    public List<Question> getQuestionsByCategory(String category) {
-        return questionDao.findByCategory(category);
+    public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionDao.findByCategory(category),HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+
     }
 
-    public String addQuestion(Question question) {
+    public ResponseEntity<String> addQuestion(Question question) {
         questionDao.save(question);
-        return "success";
+        return new ResponseEntity<>("success",HttpStatus.CREATED);
     }
 }
